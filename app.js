bd244424897207f22e840454d81eb04fb3406e90
gary <calvine.tanuwidjaja@gmail.com>
2016-12-19 17:31:27 +0700

    Add Existing file

---
+++ app.js
@@ -0,0 +1,112 @@
+var express = require('express')
+var app = express()
+var $;
+require("jsdom").env("", function(err, window) {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    $ = require("jquery")(window);
+});
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/:lat/:lon', function (req, res) {
+  var resp;
+ 	var _url = 'http://api.openweathermap.org/data/2.5/forecast?lat='+req.params.lat+'&lon='+req.params.lon+'&appid=761aeba58339a5ad8ac2b75e91ef26de&units=metric';
+ 		$.ajax({
+			url: _url,
+			type: "GET",
+			cache:false,
+			success: function(resp){
+				var listArr = [];
+				var currentDate = new Date();
+				var pointer = new Date();
+				var today = [];
+				var tomorrow = [];
+				var newList = []
+				var flag = 0;
+
+
+				for(var i = 0; i < resp.list.length; i++){
+
+					var date = new Date(resp.list[i].dt_txt);
+					if(date.getDate() == currentDate.getDate()){
+						if((date.getHours()+3) >= currentDate.getHours()){
+
+							//console.log("its Today");
+							listArr.push({day: "today", date: resp.list[i].dt_txt,temp: resp.list[i].main.temp, weather: resp.list[i].weather[0].main});
+              //console.log(today);
+						}
+					} else if(date.getDate() == (currentDate.getDate() + 1)){
+						//console.log("its tomorrow");
+						listArr.push({day: "tomorrow", date: resp.list[i].dt_txt,temp: resp.list[i].main.temp, weather: resp.list[i].weather[0].main});
+            //console.log(tomorrow);
+					} else {
+            var dateDay = date.getDay();
+            //console.log("lalalalalallalala = "+dateDay);
+            switch(dateDay){
+              case 0:
+                var thisDayName =  "Sunday";
+                break;
+              case 1:
+                var thisDayName =  "Monday";
+                break;
+              case 2:
+                var thisDayName =  "Tuesday";
+                break;
+              case 3:
+                var thisDayName =  "Wednesday";
+                break;
+              case 4:
+                var thisDayName =  "Thursday";
+                break;
+              case 5:
+                var thisDayName =  "Friday";
+                break;
+              case 6:
+                var thisDayName =  "Saturday";
+                break;
+            }
+
+						//console.log("pointer : "+pointer.getDate()+" and loop date : "+date.getDate());
+						//console.log("i : "+i+" & "+"len :"+(resp.list.length-1));
+						//if(pointer.getDate() < date.getDate()){
+						//	console.log("pointer will change");
+						//	if(flag != 0){
+						//		listArr.push(newList);
+								//console.log("pushing new list");
+						//	}
+						//	pointer = date;
+							//newList = [];
+						//	flag = 1;
+						//}
+						listArr.push({day: thisDayName, date: resp.list[i].dt_txt,temp: resp.list[i].main.temp, weather: resp.list[i].weather[0].main});
+						//if(i == (resp.list.length-1)){
+							//listArr.push(newList);
+            //  console.log(newList);
+							//console.log("pushing new list");
+						//}
+
+					}
+          console.log("listArr--------------------------------------------------------------------");
+          console.log(listArr);
+					//console.log(date);
+					//listArr.push({date: resp.list[i].dt_txt, temp: resp.list[i].main.temp, weather: resp.list[i].weather[0].main});
+				}
+				var obj = {city:resp.city.name, list:listArr};
+        //console.log("obj----------------------------------------------------------------------------");
+				//console.log(obj);
+				resp = JSON.stringify(obj);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(resp);
+			}
+		});
+
+  //console.log(resp);
+ 	//res.setHeader('Content-Type', 'application/json');
+ 	//res.send(resp);
+})
+app.listen(3000);
+
+console.log('server runing at 127.0.0.1:3000');
